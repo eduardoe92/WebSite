89dd856d3a6f1c58e7770ec6ecce11e9c2df42f3
eduardoe92 <eduardo.elias.1192@gmail.com>
2024-05-14 18:48:23 -0300

    form json

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,112 +1,3 @@
-// import React, { useState } from "react";
-// import Card from "react-bootstrap/Card";
-// import Button from "react-bootstrap/Button";
-// import Form from "react-bootstrap/Form";
-// import { BsSend } from "react-icons/bs";
-// import { Row } from "react-bootstrap";
-
-// const formFields = [
-//     { label: "Nombre:", type: "text" },
-//     { label: "Correo:", type: "email" },
-//     { label: "Mensaje:", type: "textarea" },
-// ];
-
-// function ContactForm() {
-//     const [errorMessage, setErrorMessage] = useState("");
-//     const [successMessage, setSuccessMessage] = useState("");
-
-//     const isValidEmail = (email) => {
-//         const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-//         return emailRegex.test(email);
-//     };
-
-//     const handleSubmit = async (e) => {
-//         e.preventDefault();
-//         const formData = new FormData(e.target);
-//         const nombre = formData.get("nombre");
-//         const correo = formData.get("correo");
-//         const mensaje = formData.get("mensaje");
-//         if (
-//             !nombre ||
-//             !correo ||
-//             !mensaje ||
-//             mensaje.length > 1500 ||
-//             !isValidEmail(correo)
-//         ) {
-//             setErrorMessage("Por favor, complete todos los campos correctamente.");
-//             return;
-//         }
-//         try {
-//             const response = await fetch("/send-email", {
-//                 method: "POST",
-//                 headers: {
-//                     "Content-Type": "application/json",
-//                 },
-//                 body: JSON.stringify({
-//                     nombre,
-//                     correo,
-//                     mensaje,
-//                 }),
-//             });
-//             if (response.status === 200) {
-//                 setSuccessMessage("Mensaje enviado con éxito");
-//             } else {
-//                 setErrorMessage("Hubo un problema al enviar el mensaje.");
-//             }
-//         } catch (error) {
-//             setErrorMessage("Hubo un problema al enviar el mensaje.");
-//         }
-//     };
-
-//     return (
-//         <Card className="project-card-view form-input mx-auto">
-//             <br />
-//             <Row>
-//                 <Card.Body>
-//                     <Form onSubmit={handleSubmit}>
-//                         {formFields.map((field, index) => (
-//                             <Form.Group
-//                                 key={index}
-//                                 className="d-flex align-items-center justify-content-between flex-column flex-md-row"
-//                             >
-//                                 <Form.Label className="text-left text-md-center">
-//                                     {field.label}
-//                                 </Form.Label>
-//                                 {field.type === "textarea" ? (
-//                                     <Form.Control
-//                                         as={field.type}
-//                                         rows={3}
-//                                         maxLength={1500}
-//                                         className="project-card-view-form form-user text-right"
-//                                         name={field.label.toLowerCase()}
-//                                         placeholder={`Escribe tu ${field.label.toLowerCase().replace(':', '')}`}
-//                                     />
-//                                 ) : (
-//                                     <Form.Control
-//                                         type={field.type}
-//                                         className="project-card-view-form form-user text-right"
-//                                         name={field.label.toLowerCase()}
-//                                         placeholder={`${field.label.replace(':', '')}`}
-//                                     />
-//                                 )}
-//                             </Form.Group>
-//                         ))}
-//                         <br />
-//                         {errorMessage && <p className="text-danger">{errorMessage}</p>}
-//                         {successMessage && <p className="text-success">{successMessage}</p>}
-//                         <Button type="submit">
-//                             <BsSend /> Enviar
-//                         </Button>
-//                     </Form>
-//                 </Card.Body>
-//             </Row>
-//             <br />
-//         </Card>
-//     );
-// }
-
-// export default ContactForm;
-
 import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
@@ -120,6 +11,7 @@ function ContactForm() {
   const [errorMessage, setErrorMessage] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
   const [isSending, setIsSending] = useState(false);
+  const [buttonText, setButtonText] = useState(t("form.form_pre_send"));
 
   const isValidEmail = (email) => {
     const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
@@ -129,17 +21,25 @@ function ContactForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsSending(true);
-  
+    setButtonText(t("form.form_sending"));
+
     const nombre = e.target.elements.nombre.value;
     const correo = e.target.elements.correo.value;
     const mensaje = e.target.elements.mensaje.value;
-  
-    if (!nombre || !correo || !mensaje || !isValidEmail(correo) || mensaje.length > 1500) {
+
+    if (
+      !nombre ||
+      !correo ||
+      !mensaje ||
+      !isValidEmail(correo) ||
+      mensaje.length > 1500
+    ) {
       setErrorMessage("Por favor, complete todos los campos correctamente.");
       setIsSending(false);
+      setButtonText(t("form.form_pre_send"));
       return;
     }
-  
+
     try {
       const response = await axios.post("/api/send-mail", {
         nombre,
@@ -147,17 +47,19 @@ function ContactForm() {
         mensaje,
       });
       if (response.status === 200) {
-        setSuccessMessage("Mensaje enviado con éxito");
+        setSuccessMessage(t("form.alert_send"));
         setErrorMessage("");
+        setButtonText(t("form.form_send"));
       } else {
-        setErrorMessage("Hubo un problema al enviar el mensaje.");
+        setErrorMessage(t("form.alert_fail"));
+        setButtonText(t("form.form_error"));
       }
     } catch (error) {
-      setErrorMessage("Hubo un problema al enviar el mensaje.");
+      setErrorMessage(t("form.alert_fail"));
+      setButtonText(t("form.form_error"));
     }
     setIsSending(false);
   };
-  
 
   return (
     <Card className="project-card-view form-input mx-auto">
@@ -204,8 +106,7 @@ function ContactForm() {
             {errorMessage && <p className="text-danger">{errorMessage}</p>}
             {successMessage && <p className="text-success">{successMessage}</p>}
             <Button type="submit" disabled={isSending}>
-              <BsSend />{" "}
-              {isSending ? t("form.form_sending") : t("form.form_send")}
+              <BsSend /> {buttonText}
             </Button>
           </Form>
         </Card.Body>
